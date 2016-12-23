f15c0754e1dba019a1c962cf7857b66af983d2e9
Loïc Cotonéa <loic.cotonea@sogeti.com>
2016-12-23 15:37:23 +0100

    Add NodeJS installer

---
+++ post-install/NODEJS/install_nodejs6.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo Installing NodeJS
+
+sudo echo "> Test droit sudo [OK]"
+
+# Downloading apt source list and install it
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+# Install
+sudo apt-get install -y nodejs
+
+# Upgrade NPM to last version
+sudo npm -g install npm
+
+# All done
+echo ---------------------------------------------------------------------------
+echo Node `node -v` has been installed
+echo Npm `npm -v` has been installed
+echo ---------------------------------------------------------------------------
