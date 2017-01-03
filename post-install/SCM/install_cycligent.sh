bdd3b253f703a31acc74f2c0541401d0283c678a
sogetimaitral <gregory.maitrallain@sogeti.com>
2017-01-03 11:54:39 +0100

    Update for Cycligent 0.4.2

--- post-install/SCM/install_cycligent.sh
+++ post-install/SCM/install_cycligent.sh
@@ -4,7 +4,7 @@ echo Installing Cycligent
 sudo echo "> Test droit sudo [OK]"
 
 # Download and install
-wget https://s3-us-west-2.amazonaws.com/cycligent-downloads/CycligentGitTool/installers/linux/x64/CycligentGitTool-0.4.1-linux-x64.deb -O CycligentGitTool-linux-x64.deb
+wget https://s3-us-west-2.amazonaws.com/cycligent-downloads/CycligentGitTool/installers/linux/x64/CycligentGitTool-0.4.2-linux-x64.deb -O CycligentGitTool-linux-x64.deb
 sudo dpkg -i CycligentGitTool-linux-x64.deb
 sudo apt-get install -f
 rm -f CycligentGitTool-linux-x64.deb
