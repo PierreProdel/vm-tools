73f02a05f8862f742304189d2460b20fd595670f
Gregory Maitrallain <gregory.maitrallain@sogeti.com>
2017-01-19 15:52:56 +0100

    Split vm-tools bash for easier bootstrap

--- post-install/SH/install_bash.sh
+++ post-install/SH/install_bash.sh
@@ -4,13 +4,25 @@ cp -f .bashrc_pro ~/
 
 if grep -q "INCLUDE_PRO" "$HOME/.bashrc"
 then
-  echo Bash already setup, updating file...
+  echo Pro bash already setup, updating file...
 else
   cp ~/.bashrc ~/.bashrc.orig
   echo '' >> ~/.bashrc
   echo '# INCLUDE_PRO' >> ~/.bashrc
   echo 'if [ -f $HOME/.bashrc_pro ]; then' >> ~/.bashrc
   echo '  . $HOME/.bashrc_pro' >> ~/.bashrc
+  echo 'fi' >> ~/.bashrc
+fi
+
+if grep -q "INCLUDE_VMTOOLS" "$HOME/.bashrc"
+then
+  echo VM bash already setup, updating file...
+else
+  cp ~/.bashrc ~/.bashrc.orig
+  echo '' >> ~/.bashrc
+  echo '# INCLUDE_VMTOOLS' >> ~/.bashrc
+  echo 'if [ -f $HOME/.bashrc_vmtools ]; then' >> ~/.bashrc
+  echo '  . $HOME/.bashrc_vmtools' >> ~/.bashrc
   echo 'fi' >> ~/.bashrc
 fi
 
