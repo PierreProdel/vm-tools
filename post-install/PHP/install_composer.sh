4c06e5170fad30cf57e51b3ae7e5f4530457b55a
Gregory Maitrallain <gregory.maitrallain@sogeti.com>
2017-02-21 12:27:29 +0100

    Updates composer installer to add perl dependency

--- post-install/PHP/install_composer.sh
+++ post-install/PHP/install_composer.sh
@@ -4,7 +4,7 @@ echo Installing PHP Composer
 
 sudo echo "> Test droit sudo [OK]"
 
-sudo apt-get install -y php5-cli
+sudo apt-get install -y perl php5-cli
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
