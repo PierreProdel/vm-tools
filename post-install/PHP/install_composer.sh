8a9875ae0aea9a2fc3900480ab5218e0d8d0bd05
Gregory Maitrallain <gregory.maitrallain@sogeti.com>
2017-02-21 12:22:45 +0100

    Adds PHP composer installer Updates Cycligent version

---
+++ post-install/PHP/install_composer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo Installing PHP Composer
+
+sudo echo "> Test droit sudo [OK]"
+
+sudo apt-get install -y php5-cli
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+
+# All done
+echo ---------------------------------------------------------------------------
+echo Composer is installed. Use:
+echo $ composer
+echo ---------------------------------------------------------------------------
