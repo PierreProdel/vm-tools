97d37dd70d4174d18293df712a7cb39976fe15fa
Gregory Maitrallain <gregory.maitrallain@sogeti.com>
2017-04-27 10:52:51 +0200

    Fix ZAP install

--- post-install/SECU/install_zap.sh
+++ post-install/SECU/install_zap.sh
@@ -5,9 +5,9 @@ echo Installing ZAP
 
 sudo echo -e "> Test droit sudo [\033[32mOK\033[0m]"
 
-sudo echo 'deb http://download.opensuse.org/repositories/home:/cabelo/Debian_8.0/ /' > /etc/apt/sources.list.d/owasp-zap.list 
+sudo sh -c 'echo "deb http://download.opensuse.org/repositories/home:/cabelo/Debian_8.0/ /" > /etc/apt/sources.list.d/owasp-zap.list'
 sudo apt-get update
-sudo apt-get install owasp-zap
+sudo apt-get install -y owasp-zap
 
 # Create desktop shortcut (does not work)
 echo "- Creates shortcut"
