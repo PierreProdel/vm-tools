fdfd8a989c309079824a5661f5eab74e3439a7d5
Gregory Maitrallain <gregory.maitrallain@sogeti.com>
2017-05-22 17:15:40 +0200

    Adds PHP7 repository for install

--- post-install/PHP/install_php7.sh
+++ post-install/PHP/install_php7.sh
@@ -12,7 +12,11 @@ if [ "$choice" == "y" ]; then
     sudo apt-get remove -y php5*
 fi
 
-sudo apt-get install -y php7.0-cli php7.0-redis php7.0-mongodb php7.0-curl php7.0-json php7.0-xdebug php7.0-dev php7.0-readline php7.0-mbstring
+sudo sh -c 'echo "deb http://packages.dotdeb.org jessie all" > /etc/apt/sources.list.d/dotdeb.list'
+wget -O- https://www.dotdeb.org/dotdeb.gpg | sudo apt-key add -
+sudo apt update
+
+sudo apt-get install -y php7.0-cli php7.0-redis php7.0-mongodb php7.0-curl php7.0-json php7.0-xdebug php7.0-dev php7.0-readline php7.0-mbstring php7.0-xml
 
 # All done
 echo ---------------------------------------------------------------------------
