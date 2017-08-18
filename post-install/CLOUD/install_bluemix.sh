66c663d399553b31ec992ff4d99c7164eb548cf0
Grégory Maitrallain <gregory.maitrallain@sogeti.com>
2017-08-18 15:41:46 +0200

    Updates with mandatory plugins

--- post-install/CLOUD/install_bluemix.sh
+++ post-install/CLOUD/install_bluemix.sh
@@ -5,11 +5,13 @@ echo Installing Bluemix CLI
 
 sudo echo -e "> Test droit sudo [\033[32mOK\033[0m]"
 
-#curl -fsSL https://clis.ng.bluemix.net/install/linux -o bx.sh
-#sudo sh bx.sh
-#rm -f bx.sh
+curl -fsSL https://clis.ng.bluemix.net/install/linux -o bx.sh
+sudo sh bx.sh
+rm -f bx.sh
 
 echo Installing IBM Bluemix Container Service Plug-in
 bx plugin install IBM-Containers -r Bluemix
+bx plugin install container-service -r Bluemix
+bx plugin install container-registry -r Bluemix
 
 bx plugin list
