1e13532d9b6eab242bbfbe9a9130eac70f1bb7cd
Grégory Maitrallain <gregory.maitrallain@sogeti.com>
2017-09-06 10:08:41 +0200

    Updates code installer

--- post-install/IDE/install_vscode.sh
+++ post-install/IDE/install_vscode.sh
@@ -6,7 +6,7 @@ echo Installing Visual Studio Code
 sudo echo -e "> Test droit sudo [\033[32mOK\033[0m]"
 
 # Download and install
-wget https://az764295.vo.msecnd.net/stable/f6868fce3eeb16663840eb82123369dec6077a9b/code_1.12.1-1493934083_amd64.deb -O vscode.deb
+wget https://az764295.vo.msecnd.net/stable/41abd21afdf7424c89319ee7cb0445cc6f376959/code_1.15.1-1502903936_amd64.deb -O vscode.deb
 sudo dpkg -i vscode.deb
 sudo apt-get install -f
 rm -f vscode.deb
