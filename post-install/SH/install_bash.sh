81d137750065458a47e279f31a21de2e66d51cc6
Grégory Maitrallain <gregory.maitrallain@sogeti.com>
2017-12-13 11:27:51 +0100

    Installer compatible with zsh

--- post-install/SH/install_bash.sh
+++ post-install/SH/install_bash.sh
@@ -3,6 +3,36 @@
 mkdir -p ~/.bashrc.d
 cp -f bashrc.d/*.bashrc ~/.bashrc.d/
 
+func_install_file() {
+  if [ -f $1 ]
+  then
+    if grep -q "INCLUDE_BASHRCD" "$1"
+    then
+      echo Bash already setup.
+    else
+      echo Installing Bash in $1
+      echo '' >> $1
+      echo '# INCLUDE_BASHRCD' >> $1
+      echo 'for file in ~/.bashrc.d/*.bashrc;' >> $1
+      echo 'do' >> $1
+      echo '  source "$file"' >> $1
+      echo 'done' >> $1
+    fi
+
+    if grep -q "INCLUDE_PERSO" "$1"
+    then
+      echo Perso bash already setup.
+    else
+      echo Installing Perso bash in $1
+      echo '' >> $1
+      echo '# INCLUDE_PERSO' >> $1
+      echo 'if [ -f $HOME/.bashrc_perso ]; then' >> $1
+      echo '  . $HOME/.bashrc_perso' >> $1
+      echo 'fi' >> $1
+    fi
+  fi
+}
+
 if [ -f ~/.bashrc_pro ]
 then
   rm -f ~/.bashrc_pro
@@ -13,29 +43,8 @@ then
   rm -f ~/.bashrc_vmtools
 fi
 
-if grep -q "INCLUDE_BASHRCD" "$HOME/.bashrc"
-then
-  echo Bash already setup.
-else
-  echo '' >> ~/.bashrc
-  echo '# INCLUDE_BASHRCD' >> ~/.bashrc
-  echo 'for file in ~/.bashrc.d/*.bashrc;' >> ~/.bashrc
-  echo 'do' >> ~/.bashrc
-  echo '  source "$file"' >> ~/.bashrc
-  echo 'done' >> ~/.bashrc
-fi
-
-if grep -q "INCLUDE_PERSO" "$HOME/.bashrc"
-then
-  echo Perso bash already setup...
-else
-  cp ~/.bashrc ~/.bashrc.orig
-  echo '' >> ~/.bashrc
-  echo '# INCLUDE_PERSO' >> ~/.bashrc
-  echo 'if [ -f $HOME/.bashrc_perso ]; then' >> ~/.bashrc
-  echo '  . $HOME/.bashrc_perso' >> ~/.bashrc
-  echo 'fi' >> ~/.bashrc
-fi
+func_install_file "$HOME/.bashrc"
+func_install_file "$HOME/.zshrc"
 
 # All done
 echo ---------------------------------------------------------------------------
