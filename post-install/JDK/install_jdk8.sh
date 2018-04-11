ba18a4f611a1560c7fa7dd638ace4ea4abeccb2f
Alexis LEGROS <alexis.legros@gmail.com>
2018-04-11 09:25:22 +0200

    Upgraded JDK8 minor version to 162
    Fixed issue when link already exists

--- post-install/JDK/install_jdk8.sh
+++ post-install/JDK/install_jdk8.sh
@@ -6,16 +6,17 @@ echo Installing Oracle JDK 1.8
 sudo echo -e "> Test droit sudo [\033[32mOK\033[0m]"
 
 # Downloading with licence accepting
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.tar.gz -O jdk-8u161-linux-x64.tar.gz
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.tar.gz -O jdk-8u162-linux-x64.tar.gz
 
 # Install
-tar zxvf jdk-8u161-linux-x64.tar.gz
+tar zxvf jdk-8u162-linux-x64.tar.gz
 sudo mkdir -p /usr/lib/jvm
-sudo rm -rf /usr/lib/jvm/jdk-1.8.0_161-oracle
-sudo cp -rf jdk1.8.0_161 /usr/lib/jvm/jdk-1.8.0_161-oracle
-rm -rf jdk1.8.0_161
-rm -f jdk-8u161-linux-x64.tar.gz
-sudo ln -s /usr/lib/jvm/jdk-1.8.0_161-oracle /usr/lib/jvm/jdk8
+sudo rm -rf /usr/lib/jvm/jdk-1.8.0_162-oracle
+sudo cp -rf jdk1.8.0_162 /usr/lib/jvm/jdk-1.8.0_162-oracle
+rm -rf jdk1.8.0_162
+rm -f jdk-8u162-linux-x64.tar.gz
+sudo rm -f /usr/lib/jvm/jdk8
+sudo ln -s /usr/lib/jvm/jdk-1.8.0_162-oracle /usr/lib/jvm/jdk8
 
 # Integrates Java as default JRE/JDK
 echo Updating default Java to Oracle
@@ -28,6 +29,10 @@ sudo update-alternatives --config javac
 
 # All done
 echo ---------------------------------------------------------------------------
-echo -e "To continue, edit \033[1m~/.bashrc_perso\033[0m and add or edit the following line:"
+echo -e "To continue, edit \033[1m~/.bashrc_perso\033[0m and add or edit the following lines:"
 echo -e "\033[31;1mexport JAVA_HOME=/usr/lib/jvm/jdk8\033[0m"
+echo -e "\033[31;1mexport PATH=${PATH}:${JAVA_HOME}/bin"
+echo -e "or play following commands"
+echo -e 'echo "export JAVA_HOME=/usr/lib/jvm/jdk8\" >> ~/.bashrc_perso'
+echo -e 'echo "export PATH=${PATH}:${JAVA_HOME}/bin\" >> ~/.bashrc_perso'
 echo ---------------------------------------------------------------------------
