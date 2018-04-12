4e8803cb7f3133c6cd9604e29480b21ee32c9332
pprodel <pierre.prodel@sogeti.com>
2018-04-12 18:28:45 +0200

    Change execution for jobs + add reboot for clamav update

--- post-install/TOOLS/update_vm_by_cron.sh
+++ post-install/TOOLS/update_vm_by_cron.sh
@@ -17,8 +17,9 @@ sudo chmod +x -R ${UPDATE_DIR}
 
 echo "--- ADDING NEW CRON JOBS TO ROOT CRONTAB ---" |sudo tee -a ${CRON_LOG} > /dev/null
 
-# Add job to update apt packages
-JOB1="0 0 1 * * $UPDATE_DIR/update_apt_packages.sh"
+# Add job to update apt packages (every first day of the month at 21:00)
+CRON_PERIOD1="0 21 1 * *"
+JOB1="$CRON_PERIOD1 $UPDATE_DIR/update_apt_packages.sh"
 FINDJOB1=$(sudo crontab -l | grep -F "$JOB1")
 
 if [ -z "$FINDJOB1" ]
@@ -30,14 +31,28 @@ else
   echo "APT UPDATE JOB ALREADY EXISTS" | sudo tee -a ${CRON_LOG} > /dev/null
 fi
 
-# Add job to update clamav virus definition db
-JOB2="0 0 1 * * $UPDATE_DIR/update_clamav_virus_def.sh"
+# Add job to update clamav virus definition db (every monday at 21:00)
+CRON_PERIOD2="0 21 * * 1"
+JOB2="$CRON_PERIOD2 $UPDATE_DIR/update_clamav_virus_def.sh"
 FINDJOB2=$(sudo crontab -l | grep -F "$JOB2")
 
 if [ -z "$FINDJOB2" ]
 then
   echo "ADD CLAMAV UPDATE JOB" | sudo tee -a ${CRON_LOG} > /dev/null
   (sudo crontab -l; echo "$JOB2") | sudo crontab -
+else
+  echo "CLAMAV UPDATE JOB" | sudo tee -a ${CRON_LOG} > /dev/null
+fi
+
+# Add job to update clamav virus definition db (on reboot)
+CRON_PERIOD3="@reboot"
+JOB3="$CRON_PERIOD3 $UPDATE_DIR/update_clamav_virus_def.sh"
+FINDJOB3=$(sudo crontab -l | grep -F "$JOB3")
+
+if [ -z "$FINDJOB2" ]
+then
+  echo "ADD CLAMAV UPDATE JOB" | sudo tee -a ${CRON_LOG} > /dev/null
+  (sudo crontab -l; echo "$JOB3") | sudo crontab -
 else
   echo "CLAMAV UPDATE JOB" | sudo tee -a ${CRON_LOG} > /dev/null
 fi
